f3961ecf25b4d171ea142d49f98d4e2a37722636
zeen-arify <zeenarify@gmail.com>
2022-01-26 10:22:06 +0700

    Tambah file say.java

---
+++ src/com/bariekly/javaoop/say.java
@@ -0,0 +1,12 @@
+package com.bariekly.javaoop;
+
+public class say {
+
+    void sayTahmid(){
+        System.out.println("Alhamdulillah");
+    }
+
+    public static void main(String[]args){
+
+    }
+}
