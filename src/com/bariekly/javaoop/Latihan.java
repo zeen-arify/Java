e2b721b6a7c71a8510be15b7434eba4c3db0ab9b
zeen-arify <zeenarify@gmail.com>
2022-01-26 09:46:37 +0700

    Menambahkah file Latihan.java

---
+++ src/com/bariekly/javaoop/Latihan.java
@@ -0,0 +1,8 @@
+package com.bariekly.javaoop;
+
+public class Latihan {
+    public static void main(String[]args){
+
+        System.out.printf("Assalamu alaikum");
+    }
+}
--- src/com/bariekly/javaoop/Person.java
+++ src/com/bariekly/javaoop/Person.java
@@ -1,5 +1,5 @@
 package com.bariekly.javaoop;
 
 public class Person {
-    
+
 }
